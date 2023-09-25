a84b900721e748b543c83a43dd10d64cc23c7131
bweatherly-bw <brandon.weatherly58@gmail.com>
2023-09-25 16:14:33 -0700

    completed Day1 course and Day1 Project, also created a passwordgen project to keep working on.

---
+++ Day2/bandNameGenerator.py
@@ -0,0 +1,16 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line:
+
+
+print("Let's create your new Band Name!")
+users_city = input("YO, what city did you grow up in? \n")
+users_pet = input("What is your pets name? \n")
+print("Your new Band Name could be: " + users_city + " " + users_pet)
+
---
+++ Day2/day1.py
@@ -0,0 +1,42 @@
+# Create an output to print the following text as it shows below: 
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# printing with creating a new line 
+# \n creates a New Line 
+# if you want a space before of after the 'n' then just use a space 
+
+# NO SAPCE
+print("Hello World!\nNo Space")
+
+# WITH SPACE 
+print("Hello World! \n With Space")
+
+print("Brandon\nBrandon\nBrandon\n Brandon")
+
+# String Concatination 
+print("Hello" + " " + "Brandon")
+
+# # Python Input Function 
+# IGNORE THIS >>> input("What is your name? ")
+
+# Printing the imput 
+print("Hello " + input("What is your name? "))
+# name = input('What is your name? ')
+# print(f"Your name is: {name}")
+
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+print(len(input("What is your user name? ")))
+
+# Python Variables 
+name = input("What is your name? ")
+print(name)
+
+# if you want to create a multiworded variable, seperate it out with _ 
+# ex: user_name, main_login_user
+
