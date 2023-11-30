6065d1dbc51bb5b400eacdc5dcbb43727ba6b696
bweatherly-bw <brandon.weatherly58@gmail.com>
2023-11-30 13:50:34 -0800

    created a small age to weeks calculator. This small app takes your current age as an input and lets you know the weeks that you have left to live, assuming that you live to be 90 years old.

---
+++ Day2/weeksLeft.py
@@ -0,0 +1,6 @@
+age = input("Please enter your age: ")
+
+yearsLeft = 90 - int(age)
+years_to_weeks = int(yearsLeft) * 52
+
+print(f"You have {years_to_weeks} weeks left, assuming that you live to 90 years old.")
