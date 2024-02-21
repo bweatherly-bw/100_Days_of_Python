8a687bfac6fc186b711c8b1665da6695e3767106
bweatherly-bw <brandonweatherly58@gmail.com>
2024-02-20 16:21:04 -0800

    Made a small code change to the year, also will be working on creating a more accurate age calculator for the users

--- age.py
+++ age.py
@@ -2,7 +2,12 @@
 
 name = input('What is your name? ')
 birth_year = input('What year were your born? ')
-current_year = 2022
+current_year = 2024
+
+# 
+# Could add an age calc that will tell the user exactly how old they are
+# Years, months, and days 
+# 
 
 your_age = current_year - int(birth_year)
 
